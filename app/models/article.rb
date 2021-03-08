c3463e33d7e187d5796796aeb656c3e371cb311e
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2021-03-08 14:31:22 -0500

    add one ot many association from back end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     #add validations to ensure data integrity
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 6, maximum: 100 }
     validates :description, presence: true, length: { minimum: 10, maximum: 100 }
 end
