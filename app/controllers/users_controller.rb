35df5ef41e295a115d1add448f8d78dc616e2363
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2021-03-17 15:19:46 -0400

    add user profile gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
+
     def new
         #initialize an instance variable for the view
         @user = User.new
