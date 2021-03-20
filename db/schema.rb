bc1c8f284b7799dbb5a49b81076fd6c56ef514f8
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2021-03-20 14:46:59 -0400

    admin functionality to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_192240) do
+ActiveRecord::Schema.define(version: 2020_05_01_140013) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_192240) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.boolean "admin", default: false
   end
 
   create_table "users", force: :cascade do |t|
