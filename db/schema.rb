9684bcd6f3a33705d6ca9e5ad28adb6c3457a1c3
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2021-03-25 13:58:06 -0400

    add category model and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_140013) do
+ActiveRecord::Schema.define(version: 2020_05_12_122546) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2020_05_01_140013) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
